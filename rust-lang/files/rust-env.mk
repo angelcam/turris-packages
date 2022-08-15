6777a82ab6db764787bf40aa5fa2b28e780b91ef
Ondřej Perutka <ondrej.perutka@angelcam.com>
2022-08-15 12:03:16 +0200

    Fix the Rust install dir and simplify the angelcam-connector Makefile (#5)
    * Fix the Rust install dir and simplify the angelcam-connector Makefile
    
    Rust will be installed within the build tree (the default location used to
    be user's home). As a side-result of this change, some variables common
    for rust-lang and angelcam-connector have been moved to
    rust-lang/files/rust-env.mk. This file contains also common initialization
    for Rust builds that can further simplify Makefiles for Rust apps.
    
    * CR fixes
    
    * Makefile(s) polishing (#6)
    
    * angelcam-connector: Makefile polishing
    
    - Makefile reordering to be more in sync with OpenWrt packages feed
    - Sync indentation somewhere were used spaces, somewhere not
    - Reordered install section to be more readable
    
    * rust-lang: download tarball from GitHub instead of cloning it
    
    - Download the tarball from GitHub as their releases are tagged
    It is not necessary to clone sources.
    
    - Makefile changes and reorder things to keep it up with most Makefiles
      in OpenWrt packages repository, e.g., explicitly state that rust-lang
    is the host package, it is possible to build it in parallel, etc.
    
    - Fix SPDX License Identifiers
    
    Also, it changes the approach from CONFIG_TARGET_PROFILE, because
    this was done only for one device, there is a possibility to use
    CONFIG_ARCH, which is taken from target.mk, thus anyone with ARMv7
    device can use this in OpenWrt build system. It was working for aarch64
    before, thus it is not necessary to explicitly mention it here, but
    rather be safe.
    
    * reforis-angelcam-connector-plugin: remove additional spaces
    
    These spaces were caused by text editor, but many OpenWrt Makefiles does
    not have it, so remove them.
    
    * angelcam-connector: download tarball from GitHub
    
    This approach is going to be simpler and faster, because it does not
    require to have OpenWrt build system. If you want to update your package
    because you can download the tarball, check the checksum and put it to the
    correct variable. If the hash does not match, then build system does not
    proceed anymore to compile this package.
    
    In the previous solution, you need to have a ready OpenWrt build system,
    check the PKG_MIRROR_HASH, and even if it is incorrect, then it is still
    going to proceed and checkout the sources [1].
    
    [1] https://forum.openwrt.org/t/what-pkg-mirror-hash-or-pkg-hash-means/1602/2
    
    Co-authored-by: Josef Schlehofer <pepe.schlehofer@gmail.com>

---
+++ rust-lang/files/rust-env.mk
@@ -0,0 +1,40 @@
+export RUSTUP_HOME:=$(STAGING_DIR_HOST)/lib/rustup
+export CARGO_HOME:=$(STAGING_DIR_HOST)/lib/cargo
+
+CARGO_BIN:=$(CARGO_HOME)/bin
+
+# NOTE: The Rust target triplet does not always match the target triplet used
+# by OpenWrt. You can add exceptions for your device here.
+
+# Turris MOX
+ifneq ($(findstring aarch64,$(CONFIG_ARCH)),)
+  RUST_TARGET := aarch64-unknown-linux-musl
+# Turris Omnia
+else ifneq ($(findstring arm,$(CONFIG_ARCH)),)
+  RUST_TARGET := armv7-unknown-linux-musleabihf
+# Turris 1.x
+else ifneq ($(findstring powerpc,$(CONFIG_ARCH)),)
+  RUST_TARGET := powerpc_unknown_linux_muslspe
+else
+  RUST_TARGET := $(shell echo $(CONFIG_ARCH)-unknown-linux-$(CONFIG_TARGET_SUFFIX))
+endif
+
+define CARGO_CONFIG
+[target.$(RUST_TARGET)]
+ar = "$(TARGET_AR)"
+linker = "$(TARGET_CC)"
+rustflags = [
+  "-C", "link-args=$(TARGET_LDFLAGS)",
+  "-C", "target-feature=-crt-static",
+]
+endef
+
+export CARGO_CONFIG
+
+define Build/Prepare/Rust
+	mkdir $(PKG_BUILD_DIR)/.cargo
+	echo "$$$$CARGO_CONFIG" > $(PKG_BUILD_DIR)/.cargo/config
+endef
+
+export CC_$(subst -,_,$(RUST_TARGET)):=$(TARGET_CC)
+export CFLAGS_$(subst -,_,$(RUST_TARGET)):=$(TARGET_CFLAGS)
